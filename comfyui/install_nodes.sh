ae7eddcfeb1dfaadb20bfb589cc248d2a2d12520
esc <eunso02@sookmyung.ac.kr>
2025-11-19 14:39:07 +0900

    feat: comfyui 배포

---
+++ comfyui/install_nodes.sh
@@ -0,0 +1,7 @@
+cd /app/ComfyUI/custom_nodes
+
+# ComfyUI-ComfyAPI-Nodes 설치
+echo "Installing ComfyUI-ComfyAPI-Nodes..."
+git clone https://github.com/comfyanonymous/ComfyUI-ComfyAPI-Nodes.git
+
+echo "Custom nodes installation complete."
